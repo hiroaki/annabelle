12acbf313b3e1421b551a20d6802a0ab364e366c
WATANABE Hiroaki <hwat@mac.com>
2024-06-21 13:21:14 +0900

    implement creating message

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,9 +1,13 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    logger.info("MessagesChannel#subscribed: subscribed")
+
+    stream_from 'hoge'
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    logger.info("MessagesChannel#unsubscribed: unsubscribed")
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,21 @@
 class MessagesController < ApplicationController
   def show
-    @messages = Message.all.limit(5)
+    @messages = Message.all.limit(5).order(created_at: :desc)
   end
+
+  def create
+    msg = Message.create!(message_params)
+
+    ActionCable.server.broadcast('hoge',
+      ApplicationController.renderer.render(partial: 'messages/message', locals: { message: msg })
+    )
+
+    redirect_to action: :show
+  end
+
+  private
+
+    def message_params
+        params.permit(:content)
+    end
 end
