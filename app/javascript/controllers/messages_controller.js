739c7fc528e3019ac2a395d7643e0b9815833ba8
WATANABE Hiroaki <hwat@mac.com>
2024-06-26 20:59:06 +0900

    arrange page layout

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ['preview']
+
   connect() {
     console.log('Hello, Stimulus!', this.element)
   }
@@ -8,5 +10,27 @@ export default class extends Controller {
   resetForm(evt) {
     const form = evt.target.closest('form')
     form.reset()
+  }
+
+  changePreview(evt) {
+    evt.preventDefault();
+
+    const controller = this;
+    const content_type = evt.currentTarget.dataset.type;
+    const data_url = evt.currentTarget.dataset.url;
+
+    if (content_type == 'image/jpeg' || content_type == 'image/png' || content_type == 'image/gif') {
+      controller.previewTarget.innerHTML = '<img class="fitting-width" src="'+ data_url +'">';
+    }
+    else if (content_type == 'video/mp4' || content_type == 'video/quicktime') {
+      controller.previewTarget.innerHTML = '<video class="fitting-width" src="'+ data_url +'" controls></video>';
+    }
+  }
+
+  clearPreview(evt) {
+    evt.preventDefault();
+
+    const controller = this;
+    controller.previewTarget.innerHTML = '';
   }
 }
